0cb5651b98382eaafb8e7fc8a82b11af46c65a63
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-08-06 21:52:49 +0200

    created HeaderElement structure

---
+++ src/common/HeaderElement/index.js
@@ -0,0 +1,44 @@
+import {
+    DesktopMenu,
+    Container,
+    Header,
+    UserMenuList,
+    BrandContainer,
+    Brand,
+    UserMenuItem,
+    LogoContainer,
+    Logo,
+    Navigation,
+    NavigationList,
+    NavigationItem,
+} from "./styled";
+
+export const HeaderElement = () => (
+    <Container>
+        <DesktopMenu>
+            <BrandContainer>
+                <Brand></Brand>
+            </BrandContainer>
+            <UserMenuList>
+                <UserMenuItem>Znajdź sklep</UserMenuItem>
+                <UserMenuItem>Pomoc</UserMenuItem>
+                <UserMenuItem>Dołącz do nas</UserMenuItem>
+                <UserMenuItem>Zaloguj się</UserMenuItem>
+            </UserMenuList>
+        </DesktopMenu>
+        <Header>
+            <LogoContainer>
+                <Logo></Logo>
+            </LogoContainer>
+            <Navigation>
+                <NavigationList>
+                    <NavigationItem>Nowe i polecane</NavigationItem>
+                    <NavigationItem>Mężczyźni</NavigationItem>
+                    <NavigationItem>Kobiety</NavigationItem>
+                    <NavigationItem>Dzieci</NavigationItem>
+                    <NavigationItem>Wyprzedaż</NavigationItem>
+                </NavigationList>
+            </Navigation>
+        </Header>
+    </Container>
+);
---
+++ src/common/HeaderElement/styled.js
@@ -0,0 +1,36 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    max-width: 1920px;
+    margin: 0 auto;
+`;
+
+export const DesktopMenu = styled.aside`
+    height: 36px;
+    width: 100%;
+    background-color: black;
+`;
+
+export const BrandContainer = styled.div``;
+
+export const Brand = styled.a``;
+
+export const UserMenuList = styled.ul``;
+
+export const UserMenuItem = styled.li``;
+
+export const Header = styled.header`
+    height: 64px;
+    width: 100%;
+    background-color: red;
+`;
+
+export const LogoContainer = styled.div``;
+
+export const Logo = styled.a``;
+
+export const Navigation = styled.nav``;
+
+export const NavigationList = styled.ul``;
+
+export const NavigationItem = styled.li``;
