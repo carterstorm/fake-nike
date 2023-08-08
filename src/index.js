374a13458ac8ee8abe7ca0044a803c0817175c43
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-08-08 21:33:41 +0200

    created store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux'
 import App from './App';
 import { GlobalStyle } from './GlobalStyle';
-import { theme } from "./theme";
-import { ThemeProvider } from 'styled-components';
+import { theme } from './theme';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
