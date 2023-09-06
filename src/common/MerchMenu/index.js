6397cc11070cc23b50d10c94be3ac18ff617aebd
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-06 21:53:00 +0200

    changed function name to handleMouseMenuVisibility

--- src/common/MerchMenu/index.js
+++ src/common/MerchMenu/index.js
@@ -1,5 +1,8 @@
 import { useState } from "react";
 import { useGetData } from "../../hooks/useGetData";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
+import { theme } from "../../theme";
+
 import {
     Category,
     Heading,
@@ -12,15 +15,20 @@ import {
 export const MerchMenu = ({ fetchLink }) => {
     const { state, data } = useGetData(fetchLink, 1);
     const [visibleMenu, setVisibleMenu] = useState(false);
+    const windowWidth = useWindowWidth();
 
-    const changeVisibility = () => {
-        setVisibleMenu(visibleMenu => !visibleMenu);
+    const handleMouseMenuVisibility = () => {
+        if (windowWidth > theme.media.mobile) {
+            setVisibleMenu(visibleMenu => !visibleMenu);
+        } else {
+            setVisibleMenu(false);
+        };
     };
 
     return (
         <Wrapper
-            onMouseEnter={changeVisibility}
-            onMouseLeave={changeVisibility}
+            onMouseEnter={handleMouseMenuVisibility}
+            onMouseLeave={handleMouseMenuVisibility}
         >
             {state === "loading" ?
                 (<>Loading</>)
