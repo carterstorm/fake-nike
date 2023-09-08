0a3ecf84250daaec2dca0cb8796280e6935037d8
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-09 00:58:06 +0200

    created footerData.js

---
+++ src/features/Footer/footerData.js
@@ -0,0 +1,34 @@
+export const asideOptions = [
+    { id: 1, itemText: "Karty upominkowe" },
+    { id: 2, itemText: "Znajdź sklep" },
+    { id: 3, itemText: "Nike journal" },
+    { id: 4, itemText: "Zostań członkiem" },
+    { id: 5, itemText: "Rabat dla studentów" },
+    { id: 6, itemText: "Prześlij opinię" },
+    { id: 7, itemText: "Kody promocyjne" },
+];
+
+export const helpOptions = [
+    { id: 1, itemText: "Uzyskaj pomoc" },
+    { id: 2, itemText: "Status zamówienia" },
+    { id: 3, itemText: "Wysyłka i dostawa" },
+    { id: 4, itemText: "Zwroty" },
+    { id: 5, itemText: "Opcje płatności" },
+    { id: 6, itemText: "Skontaktuj się z nami" },
+];
+
+export const aboutNike = [
+    { id: 1, itemText: "O Nike" },
+    { id: 2, itemText: "Aktualności" },
+    { id: 3, itemText: "Praca" },
+    { id: 4, itemText: "Inwestorzy" },
+    { id: 5, itemText: "Ochrona środowiska" },
+];
+
+export const joinUs = [
+    { id: 1, itemText: "Dołącz do nas" },
+    { id: 2, itemText: "Nike App" },
+    { id: 3, itemText: "Nike Run Club" },
+    { id: 4, itemText: "Nike Training Club" },
+    { id: 5, itemText: "SNKRS" },
+];
