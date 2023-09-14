058781c922567cfdd167bc059c7a600d87b4c2ca
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-14 12:15:50 +0200

    edited footerData

--- src/features/Footer/footerData.js
+++ src/features/Footer/footerData.js
@@ -1,34 +1,37 @@
-export const asideOptions = [
-    { id: 1, itemText: "Karty upominkowe" },
-    { id: 2, itemText: "Znajdź sklep" },
-    { id: 3, itemText: "Nike journal" },
-    { id: 4, itemText: "Zostań członkiem" },
-    { id: 5, itemText: "Rabat dla studentów" },
-    { id: 6, itemText: "Prześlij opinię" },
-    { id: 7, itemText: "Kody promocyjne" },
+export const titleAsideOptions = [
+    { id: 1, title: "Karty upominkowe" },
+    { id: 2, title: "Znajdź sklep" },
+    { id: 3, title: "Nike journal" },
+    { id: 4, title: "Zostań członkiem" },
+    { id: 5, title: "Rabat dla studentów" },
+    { id: 6, title: "Prześlij opinię" },
+    { id: 7, title: "Kody promocyjne" },
 ];
 
-export const helpOptions = [
-    { id: 1, itemText: "Uzyskaj pomoc" },
-    { id: 2, itemText: "Status zamówienia" },
-    { id: 3, itemText: "Wysyłka i dostawa" },
-    { id: 4, itemText: "Zwroty" },
-    { id: 5, itemText: "Opcje płatności" },
-    { id: 6, itemText: "Skontaktuj się z nami" },
+export const titleOptions = [
+    { id: 1, title: "Uzyskaj pomoc" },
+    { id: 2, title: "O Nike" },
+    { id: 3, title: "Dołącz do nas" },
 ];
 
-export const aboutNike = [
-    { id: 1, itemText: "O Nike" },
-    { id: 2, itemText: "Aktualności" },
-    { id: 3, itemText: "Praca" },
-    { id: 4, itemText: "Inwestorzy" },
-    { id: 5, itemText: "Ochrona środowiska" },
+export const subTitleHelpOptions = [
+    { id: 1, title: "Status zamówienia" },
+    { id: 2, title: "Wysyłka i dostawa" },
+    { id: 3, title: "Zwroty" },
+    { id: 4, title: "Opcje płatności" },
+    { id: 5, title: "Skontaktuj się z nami" },
 ];
 
-export const joinUs = [
-    { id: 1, itemText: "Dołącz do nas" },
-    { id: 2, itemText: "Nike App" },
-    { id: 3, itemText: "Nike Run Club" },
-    { id: 4, itemText: "Nike Training Club" },
-    { id: 5, itemText: "SNKRS" },
+export const subTitleAboutNike = [
+    { id: 1, title: "Aktualności" },
+    { id: 2, title: "Praca" },
+    { id: 3, title: "Inwestorzy" },
+    { id: 4, title: "Ochrona środowiska" },
+];
+
+export const subTitleJoinUs = [
+    { id: 1, title: "Nike App" },
+    { id: 2, title: "Nike Run Club" },
+    { id: 3, title: "Nike Training Club" },
+    { id: 4, title: "SNKRS" },
 ];
