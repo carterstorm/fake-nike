222d3170f52ef8a6149374dff10de24dae43cb99
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-09-16 15:41:23 +0200

    added states to Slider component

--- src/common/Slider/index.js
+++ src/common/Slider/index.js
@@ -2,40 +2,50 @@ import { useGetData } from "../../hooks/useGetData";
 import { useSliderIndex } from "../../hooks/useSliderIndex";
 import { Arrow } from "../Arrow";
 import { HeaderHeading } from "../HeaderHeading";
+import { Items } from "./Items";
 
 import {
     Wrapper
 } from "./styled"
-import { Items } from "./Items";
 
 export const Slider = ({ heading, fetchLink, numberOfItems }) => {
-    const { data } = useGetData(fetchLink, 1);
+    const { state, data } = useGetData(fetchLink, 1);
     const [index, setPrevIndex, setNextIndex] = useSliderIndex(data);
 
     return (
         <Wrapper>
-            <HeaderHeading
-                heading={heading}
-                membership
-            />
-            <Arrow
-                direction={"left"}
-                left={"30"}
-                onClick={() => setPrevIndex()}
-            />
-            <Items
-                index={index}
-                fetchLink={fetchLink}
-                numberOfItems={numberOfItems}
-                data={data}
-                setPrevIndex={setPrevIndex}
-                setNextIndex={setNextIndex}
-            />
-            <Arrow
-                direction={"right"}
-                right={"30"}
-                onClick={() => setNextIndex()}
-            />
+            {state === "loading" ?
+                (<>Loading</>)
+                :
+                state === "error" ?
+                    (<>Error</>)
+                    :
+                    (
+                        <>
+                            <HeaderHeading
+                                heading={heading}
+                                membership
+                            />
+                            <Arrow
+                                direction={"left"}
+                                left={"30"}
+                                onClick={() => setPrevIndex()}
+                            />
+                            <Items
+                                index={index}
+                                fetchLink={fetchLink}
+                                numberOfItems={numberOfItems}
+                                data={data}
+                                setPrevIndex={setPrevIndex}
+                                setNextIndex={setNextIndex}
+                            />
+                            <Arrow
+                                direction={"right"}
+                                right={"30"}
+                                onClick={() => setNextIndex()}
+                            />
+                        </>
+                    )}
         </Wrapper>
     );
 };
